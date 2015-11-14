71693774370f916066fe628920eda4f06bc7fbdc
Matthew Tomaziefski <matthew@tomaziefski.com>
2015-11-14 16:16:55 -0500

    update

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     	<script src="js/jefferson-changer.js" type="text/javascript"></script>
     	<script src="js/main.js" type="text/javascript"></script>
+        <script src="js/swap.js" type="text/javascript"></script>
     </body>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -31,50 +31,5 @@ if(img.length > 0){
         img.css('-o-transform', 'rotate('+degree+'deg)');
         img.css('-ms-transform', 'rotate('+degree+'deg)');
     }
-
     $(document).mousemove(mouse);
-
-    /* Declaring the global variables */
-            var imageOne;
-
-            /* Calling the initialization function */
-            $(init);
-
-            /* The images need to re-initialize on load and on resize, or else the areas
-             * where each image is displayed will be wrong. */
-            $(window).load(init);
-            $(window).resize(init);
-
-            /* Setting the mousemove event caller */
-            $(window).mousemove(getMousePosition);
-
-            /* This function is called on document ready, on load and on resize
-             * and initiallizes all the images */
-            function init(){
-                
-                /* Instanciate the mouse position variables */
-                mouseX = 600;
-                mouseY = 300;
-                
-                /* Instanciate a HeadImage class for every image */
-                imageOne = new HeadImage("one");
-            }
-
-            /* This function is called on mouse move and gets the mouse position. 
-             * It also calls the HeadImage function to display the correct image*/
-            function getMousePosition(event){
-                
-                /* Setting the mouse position variables */
-                mouseX = event.pageX;
-                mouseY = event.pageY;
-                
-                /*Calling the setImageDirection function of the HeadImage class
-                 * to display the correct image*/
-                imageOne.setImageDirection();
-            }
-
-            //Get mouse position on ready
-            $(document).ready(getMousePosition);
 }
-
-            
